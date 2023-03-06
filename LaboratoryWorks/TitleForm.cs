5499973c4cb024f32a2078c0196ff275e4e00a68
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-06 18:34:13 +0300

    Upload second laboratory work

---
+++ LaboratoryWorks/TitleForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LaboratoryWorks
+{
+    public partial class TitleForm : Form
+    {
+        public TitleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Laboratory2TitleForm nextForm = new Laboratory2TitleForm();
+            nextForm.Show();
+
+        }
+    }
+}
